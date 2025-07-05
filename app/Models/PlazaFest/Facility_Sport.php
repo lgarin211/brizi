c5b670e3bf33b69330fec35b0836b0e747f43810
Doni Arifitranto <doniarifitrantoo@gmail.com>
2025-07-05 16:12:14 +0700

    modified:   app/Models/PlazaFest/Facility_Sport.php

--- app/Models/PlazaFest/Facility_Sport.php
+++ app/Models/PlazaFest/Facility_Sport.php
@@ -5,7 +5,7 @@ namespace App\Models\PlazaFest;
 use Illuminate\Database\Eloquent\Model;
 
 
-class Facility extends Model
+class Sport_Facility extends Model
 {
     protected $table = 'sport_facility';
 }
