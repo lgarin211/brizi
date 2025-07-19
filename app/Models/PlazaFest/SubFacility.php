b39c73a95a861d07deb551812504e92e4a9d04c0
lgarin211 <lagustinus211@gmail.com>
2025-07-19 15:09:52 +0700

    modified:   app/Models/PlazaFest/SubFacility.php

--- app/Models/PlazaFest/SubFacility.php
+++ app/Models/PlazaFest/SubFacility.php
@@ -3,8 +3,12 @@
 namespace App\Models\PlazaFest;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class SubFacility extends Model
 {
-    protected $table = 'sub_facility';}
+    use SoftDeletes;
+    protected $table = 'sub_facility';
+    protected $dates = ['deleted_at'];
+}
