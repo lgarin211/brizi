3c6372a8b07d37ffb7292de93a1c988e235fba0d
lgarin211 <lagustinus211@gmail.com>
2025-07-27 18:35:40 +0700

    modified:   resources/views/vendor/voyager/partials/app-footer.blade.php

--- resources/views/vendor/voyager/partials/app-footer.blade.php
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="app-footer">
     <div class="site-footer-right">
-        @if (rand(1,100) == 100)
+        {{-- @if (rand(1,100) == 100)
             <i class="voyager-rum-1"></i> {{ __('voyager::theme.footer_copyright2') }}
         @else
             {!! __('voyager::theme.footer_copyright') !!} <a href="http://thecontrolgroup.com" target="_blank">The Control Group</a>
@@ -8,6 +8,8 @@
         @php $version = Voyager::getVersion(); @endphp
         @if (!empty($version))
             - {{ $version }}
-        @endif
+        @endif --}}
+
+        System Build Power with <a href="https://www.progesio.my.id">Progesio</a>
     </div>
 </footer>
