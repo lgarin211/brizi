4ca8bd8265a21c9d488edf12e1c1db28a765cbd1
lgarin211 <lagustinus211@gmail.com>
2025-08-09 04:36:12 +0700

    modified:   app/Http/Controllers/PlazaFest/WelcomeController.php

--- app/Http/Controllers/PlazaFest/WelcomeController.php
+++ app/Http/Controllers/PlazaFest/WelcomeController.php
@@ -9,67 +9,155 @@ class WelcomeController extends Controller
 {
     public function index()
     {
-        $datacomponent0 = DB::table('sliders')->get();
-        $allbanners=[];
-        foreach ($datacomponent0 as $slide) {
-            $slide->banner = url('/storage/' . $slide->banner);
-            $allbanners[] = $slide->banner;
-        }
+        $components = [
+            'component0' => $this->getSliders(),
+            'component1' => $this->getLandingData(),
+            'component2' => $this->getEvents(),
+            'component3' => $this->getSportFacilities(),
+            'component4' => $this->getTenants(),
+            'component5' => $this->getSocialMedia(),
+            'component6' => $this->getFloorMaps(),
+            'component7' => $this->getBookingImages(),
+        ];
 
-        $datacomponent1 = [
+        return response()->json([
+            'message' => 'Data berhasil diambil',
+            'data' => $components
+        ], 200);
+    }
+
+    /**
+     * Get sliders data with processed banner URLs
+     */
+    private function getSliders()
+    {
+        $sliders = DB::table('sliders')->get();
+
+        return $sliders->map(function ($slider) {
+            $slider->banner = $this->generateStorageUrl($slider->banner);
+            return $slider;
+        });
+    }
+
+    /**
+     * Get landing page data including banners
+     */
+    private function getLandingData()
+    {
+        $sliders = $this->getSliders();
+        $banners = $sliders->pluck('banner')->toArray();
+
+        return [
             'title' => setting('landing.welcome_text'),
-            'big_banner' => $allbanners,
+            'big_banner' => $banners,
             'big_welcome' => setting('landing.big_welcome')
         ];
-        $datacomponent2 = DB::table('event')
-            ->select('id', 'banner','title', 'description','created_at', 'updated_at')
-            ->get();
-        foreach ($datacomponent2 as $event) {
-            $event->banner = url('/storage/' . $event->banner);
-        }
+    }
 
+    /**
+     * Get events with processed banner URLs
+     */
+    private function getEvents()
+    {
+        return $this->getDataWithImageProcessing(
+            'event',
+            ['id', 'banner', 'title', 'description', 'created_at', 'updated_at'],
+            'banner'
+        );
+    }
 
-        $datacomponent3 = DB::table('sport_facility')
-            ->select('id','image','description','created_at','updated_at')
-            ->get();
-        foreach ($datacomponent3 as $event) {
-            $event->image = url('/storage/' . $event->image);
-        }
-        $datacomponent4 = DB::table('tenants')
-            ->select('id', 'img','location', 'created_at', 'updated_at')
-            ->get();
+    /**
+     * Get sport facilities with processed image URLs
+     */
+    private function getSportFacilities()
+    {
+        return $this->getDataWithImageProcessing(
+            'sport_facility',
+            ['id', 'image', 'description', 'created_at', 'updated_at'],
+            'image'
+        );
+    }
 
-        foreach ($datacomponent4 as $tenan) {
-            $tenan->img = url('/storage/' . $tenan->img);
-        }
+    /**
+     * Get tenants with processed image URLs
+     */
+    private function getTenants()
+    {
+        return $this->getDataWithImageProcessing(
+            'tenants',
+            ['id', 'img', 'location', 'created_at', 'updated_at'],
+            'img'
+        );
+    }
 
-        $datacomponent5 = [
+    /**
+     * Get social media settings
+     */
+    private function getSocialMedia()
+    {
+        return [
             'fb' => setting('landing.fb'),
             'ig' => setting('landing.ig')
         ];
+    }
 
-        $datacomponent6 = [
+    /**
+     * Get floor maps with processed URLs
+     */
+    private function getFloorMaps()
+    {
+        $maps = [
             setting('lantai.ug_map'),
             setting('lantai.gf_map')
         ];
 
-        foreach ($datacomponent6 as $key => $map) {
-            if ($map) {
-                $datacomponent6[$key] = url('/storage/' . $map);
-            }
-        }
+        return array_map(function ($map) {
+            return $map ? $this->generateStorageUrl($map) : null;
+        }, array_filter($maps));
+    }
 
+    /**
+     * Get booking images that are currently active
+     */
+    private function getBookingImages()
+    {
+        $images = DB::table('img_dbooking')
+            ->where('start_show', '<=', now())
+            ->where('end_show', '>=', now())
+            ->get();
 
+        return $images->map(function ($image) {
+            $image->image = $this->generateStorageUrl($image->image);
+            return $image;
+        });
+    }
 
-        $response = [
-            'component0' => $datacomponent0,
-            'component1' => $datacomponent1,
-            'component2' => $datacomponent2,
-            'component3' => $datacomponent3,
-            'component4' => $datacomponent4,
-            'component5' => $datacomponent5,
-            'component6' => $datacomponent6
-        ];
-        return response()->json(['message' => 'Data berhasil diambil', 'data' => $response], 200);
+    /**
+     * Generic method to get data with image processing
+     *
+     * @param string $table
+     * @param array $columns
+     * @param string $imageColumn
+     * @return \Illuminate\Support\Collection
+     */
+    private function getDataWithImageProcessing($table, $columns, $imageColumn)
+    {
+        $data = DB::table($table)->select($columns)->get();
+
+        return $data->map(function ($item) use ($imageColumn) {
+            $item->$imageColumn = $this->generateStorageUrl($item->$imageColumn);
+            return $item;
+        });
+    }
+
+    /**
+     * Generate storage URL for given path
+     *
+     * @param string $path
+     * @return string
+     */
+    private function generateStorageUrl($path)
+    {
+        return url('/storage/' . $path);
     }
 }
